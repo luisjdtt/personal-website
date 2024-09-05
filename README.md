c34e80968561b2ead23b96bcf1278633dab5ed4a
Luis De Trejo <luisdetrejo@rodai.mx>
2024-09-04 20:35:23 -0600

    Fix animation integration with animate.css and viewport detection. Add 404 and add alternate link for multilingual search engine optimizations

--- README.md
+++ README.md
@@ -21,3 +21,27 @@ Run `npm run build` to build the project 🏗️
 ## Eleventy 🚀
 
 You need to understand the basics of Eleventy to customize this website. Understand layout, data, and templates. 📚
+
+## Jampack 📦
+
+Jampack is a tool to optimize static websites for best user experience and best Core Web Vitals scores.
+
+It is a postprocessing tool that takes the output of the ssg (eleventy) and optimizes the website for best user experience and best Core Web Vitals scores.
+
+Check the docs [here](https://jampack.divriots.com/) for more information and possible configurations.
+
+It is important to test the website locally after optimization because it is not 100% accurate. After running `npm run build`, test the webiste using something like http-server to make sure that the website is working as expected.
+
+```bash
+npx http-server _site
+```
+
+`npm run build` already has the jampack command.
+
+## Things to do
+
+- [X] Add proper meta alternative tags for SEO due to having multiple languages.
+- [X] Fix animations using proper s_ prefix 
+- [X] Add 404 page
+- [ ] Add a Google Drive link to download my resume
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,101 @@
-function aaosInit(anchor) {
-  let aaosClassName = "as";
-  let options;
-  if (anchor && typeof anchor == "object") options = anchor;
+// function aaosInit(anchor) {
+//   let aaosClassName = "as";
+//   let options;
+//   if (anchor && typeof anchor == "object") options = anchor;
+//   else
+//     options = {
+//       root: null, // Use the viewport as the root
+//       rootMargin: "0px",
+//       threshold: 0.1,
+//     };
+//   if (options && options.aaosClassName) aaosClassName = options.aaosClassName;
+
+//   let observer = new IntersectionObserver(callback, options);
+//   let targets = document.getElementsByClassName(aaosClassName);
+//   for (let target of targets) {
+//     prepareElement(target);
+//     observer.observe(target);
+//   }
+
+//   function callback(entries, observer) {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         animateElement(entry.target);
+//       }
+//     });
+//   }
+
+//   function prepareElement(target) {
+//     var classList = target.classList;
+//     for (let i = 0; i < classList.length; i++) {
+//       if (
+//         classList[i].startsWith("animate__") &&
+//         !classList[i].startsWith("animate__animated")
+//       ) {
+//         let originalClass = "s_" + classList[i];
+//         target.classList.remove(classList[i]);
+//         target.classList.add(originalClass);
+//       }
+//     }
+//   }
+
+//   function animateElement(target) {
+//     var classList = target.classList;
+//     for (let i = 0; i < classList.length; i++) {
+//       if (classList[i].startsWith("s_animate__")) {
+//         let animationClass = classList[i].substring(2);
+//         target.classList.remove(classList[i]);
+//         target.classList.add(animationClass);
+//       }
+//     }
+//   }
+// }
+
+function aaosInit(anchor){
+  let aaosClassName = 'as'
+  let options
+  if (anchor && (typeof anchor)=='object')
+    options = anchor
   else
-    options = {
-      root: null, // Use the viewport as the root
-      rootMargin: "0px",
-      threshold: 0.1,
-    };
-  if (options && options.aaosClassName) aaosClassName = options.aaosClassName;
+	 options = {
+		root: document.querySelector(anchor || document.body),
+		rootMargin: '0px',
+		threshold: 0.9
+	}
+  if (options && options.aaosClassName) aaosClassName = options.aaosClassName
+  if (options && options.root && (typeof options.root)=='string') options.root = document.querySelector(options.root)
 
-  let observer = new IntersectionObserver(callback, options);
-  let targets = document.getElementsByClassName(aaosClassName);
-  for (let target of targets) {
-    prepareElement(target);
-    observer.observe(target);
-  }
+	let observer = new IntersectionObserver(callback, options);
+  let targets = document.getElementsByClassName(aaosClassName)
+	for (var i = 0; i < targets.length; i++) 
+		observer.observe(targets[i]);
 
-  function callback(entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        animateElement(entry.target);
-      }
-    });
-  }
+	function callback(entries, observer) {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) 
+				replaceClass(entry.target)
+		});
+	};
 
-  function prepareElement(target) {
-    var classList = target.classList;
-    for (let i = 0; i < classList.length; i++) {
-      if (classList[i].startsWith("animate__")) {
-        let originalClass = "s_" + classList[i];
-        target.classList.remove(classList[i]);
-        target.classList.add(originalClass);
-      }
-    }
-  }
-
-  function animateElement(target) {
-    var classList = target.classList;
-    for (let i = 0; i < classList.length; i++) {
-      if (classList[i].startsWith("s_animate__")) {
-        let animationClass = classList[i].substring(2);
-        target.classList.remove(classList[i]);
-        target.classList.add(animationClass, "animate__animated");
-      }
-    }
-  }
+	function replaceClass(target){
+		var classList = target.classList
+		for (var i = 0; i < classList.length; i++) {
+		 if (classList[i].startsWith('s_animate')){
+			 var klass = classList[i]
+			 target.classList.remove(classList[i])
+			 target.classList.add(klass.substring(2))
+			 target.classList.remove(aaosClassName)
+		 }
+		}
+	}	
 }
 
 // Usage
-document.addEventListener("DOMContentLoaded", function () {
-  aaosInit({
-    rootMargin: "0px",
-    threshold: 0.01,
-    aaosClassName: "as",
-  });
+// document.addEventListener("DOMContentLoaded", function () {
+aaosInit({
+  root: null, // Use the viewport as the root
+  rootMargin: "0px",
+  threshold: 0.01,
+  aaosClassName: "as",
 });
+// });
